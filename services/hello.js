485cd718777e33f25e9ced8309cb5b90d75ae885
AntSworD <zhengjj.asd@gmail.com>
2017-07-10 14:04:50 +0800

    test ekitchen

--- services/hello.js
+++ services/hello.js
@@ -2,5 +2,6 @@
 require('dotenv').config();
 
 exports.handler = (event, ctx, callback) => {
+  console.log('invoke fc_helloworld!');
   callback(null, `Hello world! NODE_ENV: ${process.env.NODE_ENV}`);
 };
