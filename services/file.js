b3b803d0b11ba61c0774a583226afc0fb2a79cfe
AntSworD <zhengjj.asd@gmail.com>
2017-07-13 18:39:42 +0800

    demo: download file

---
+++ services/file.js
@@ -0,0 +1,15 @@
+'use strict';
+const path = require('path');
+const fs = require('fs');
+
+exports.handler = (event, ctx, callback) => {
+  console.log('invoke file!');
+  callback(null, {
+    isBase64Encode: false,
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/javascript'
+    },
+    body: fs.readFileSync(path.resolve(__dirname, './file.js'));
+  });
+};
